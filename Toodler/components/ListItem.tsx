e71b3a359248163b79acc2f339d0ffa458c3ec5e
Hlynur Isak <isakvillason@hotmail.com>
2024-11-28 23:55:31 +0000

    Fix ts typing errors. AddTaskModal in ListItem.tsx is missing the onSave function input

--- Toodler/components/ListItem.tsx
+++ Toodler/components/ListItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import AddTaskModal from './AddTaskModal';
